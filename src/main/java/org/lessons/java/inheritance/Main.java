f3bee6344c93262b095f0134d9c3e79e2e8dee0b
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-19 17:53:53 +0200

    add: debug in main class

--- src/main/java/org/lessons/java/inheritance/Main.java
+++ src/main/java/org/lessons/java/inheritance/Main.java
@@ -5,6 +5,25 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Prodotto prod1 = new Prodotto("name", "brand", 15.00f, 22);
+
+		System.out.println(prod1.getBrand());
+		System.out.println(prod1.getPrice());
+		System.out.println(prod1.getProductCode());
+		System.out.println(prod1.getProductName());
+		System.out.println(prod1.getVat());
+
+		prod1.setBrand("brand2");
+		prod1.setPrice(222.5f);
+		prod1.setProductName("name2");
+		prod1.setVat(22);
+
+		System.out.println(prod1.getBrand());
+		System.out.println(prod1.getPrice());
+		System.out.println(prod1.getProductCode());
+		System.out.println(prod1.getProductName());
+		System.out.println(prod1.getVat());
+
 	}
 
 }
