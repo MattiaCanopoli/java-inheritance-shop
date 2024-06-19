31a23381a86c27f377f4bf78d5a22d26e439d4ba
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-19 22:43:29 +0200

    add: televisore class
    added instance varibles dispaySize and smart,
    overloaded constructor,
    getter and setters,
    getProductInfo method

--- src/main/java/org/lessons/java/inheritance/Televisore.java
+++ src/main/java/org/lessons/java/inheritance/Televisore.java
@@ -2,9 +2,44 @@ package org.lessons.java.inheritance;
 
 public class Televisore extends Prodotto {
 
+	private float displaySize;
+	private boolean smart;
+
 	public Televisore(String productName, String brand, float price, int vat) {
 		super(productName, brand, price, vat);
 		// TODO Auto-generated constructor stub
 	}
 
+	public Televisore(String productName, String brand, float price, int vat, float displaySize, boolean smart) {
+		super(productName, brand, price, vat);
+		this.displaySize = displaySize;
+		this.smart = smart;
+	}
+
+	public float getDisplaySize() {
+		return displaySize;
+	}
+
+	public void setDisplaySize(float displaySize) {
+		this.displaySize = displaySize;
+	}
+
+	public boolean isSmart() {
+		return smart;
+	}
+
+	public void setSmart(boolean smart) {
+		this.smart = smart;
+	}
+
+	public void getProductInfo() {
+		super.getProductInfo();
+		System.out.println(String.format("Dimensione: %.1f\"", this.displaySize));
+		if (this.isSmart()) {
+			System.out.println("Smart: Yes");
+		} else {
+			System.out.println("Smart: No");
+		}
+		System.out.println("************************************");
+	}
 }
