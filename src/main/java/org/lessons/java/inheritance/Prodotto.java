6469debdb6dc40e0440ae2597bfae17288ad0d05
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-19 17:53:33 +0200

    add: Prodotto class instance variables, constructor, getter and setters

--- src/main/java/org/lessons/java/inheritance/Prodotto.java
+++ src/main/java/org/lessons/java/inheritance/Prodotto.java
@@ -1,5 +1,55 @@
 package org.lessons.java.inheritance;
 
 public class Prodotto {
+	private int productCode;
+	private String productName;
+	private String brand;
+	private float price;
+	private int vat;
+
+	public Prodotto(String productName, String brand, float price, int vat) {
+		this.productName = productName;
+		this.brand = brand;
+		this.price = price;
+		this.vat = vat;
+		this.productCode = Utils.randomCode(99999999, 1);
+
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getVat() {
+		return vat;
+	}
+
+	public void setVat(int vat) {
+		this.vat = vat;
+	}
+
+	public int getProductCode() {
+		return productCode;
+	}
 
 }
