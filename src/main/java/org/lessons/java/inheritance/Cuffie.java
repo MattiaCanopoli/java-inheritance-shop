3e5208be75e5df8bdf4ecb6ab197ebb4eb4f7ff1
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-19 22:43:51 +0200

    add: overloaded constructor

--- src/main/java/org/lessons/java/inheritance/Cuffie.java
+++ src/main/java/org/lessons/java/inheritance/Cuffie.java
@@ -2,9 +2,18 @@ package org.lessons.java.inheritance;
 
 public class Cuffie extends Prodotto {
 
+	String color;
+	boolean wifi;
+
 	public Cuffie(String productName, String brand, float price, int vat) {
 		super(productName, brand, price, vat);
 		// TODO Auto-generated constructor stub
+	}
+
+	public Cuffie(String productName, String brand, float price, int vat, String color, boolean wifi) {
+		super(productName, brand, price, vat);
+		this.color = color;
+		this.wifi = wifi;
 	}
 
 }
